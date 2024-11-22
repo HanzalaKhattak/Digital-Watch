5ade5b0ad56080403bd88f1ca24fa7c75e4d3866
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-11-22 14:54:46 +0500

    Add javascript code to display current date

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ function updateClock(){
     const seconds = currentMoment.getSeconds().toString().padStart(2, '0');
 
     document.getElementById('digital-watch').innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = days[currentMoment.getDate()];
+
+    days.forEach(day => {
+        const dayElement = document.getElementById('day');
+        if(day === currentDay){
+            dayElement.classList.add('current');
+        }else{
+            dayElement.classList.remove('current');
+        }
+    });
 }
+
 setInterval(updateClock, 1000);
 updateClock();
